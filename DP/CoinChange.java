602c34a5a87eb6108c9c64f056ffa8a0d27779ee
Sri Sushma Karra <karrasrisushma@gmail.com>
2020-06-28 21:05:37 +0530

    DP

---
+++ DP/CoinChange.java
@@ -0,0 +1,14 @@
+package DP;
+
+public class CoinChange {
+	
+	int numberOfWaysToFormN(int N, int[] arr) {
+		return N;
+		
+	}
+	
+	public static void main(String[] args) {
+		numberOfWaysToFormN(9, new int[{}])
+	}
+
+}
